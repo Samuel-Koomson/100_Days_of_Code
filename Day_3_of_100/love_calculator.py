6ba1ad65950e91a5522ed950a0712a076b478f55
Samuel <kskoomson1@gmail.com>
2023-05-02 17:43:09 +0000

    I have unfinished business to sort with this file, I will come back to it

---
+++ Day_3_of_100/love_calculator.py
@@ -0,0 +1,30 @@
+# Simple python program that creates a love calculator
+
+# Don't change the code below
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# Don't change the code above
+
+#Write your code below this line 
+new_name1 = 0
+new_name2 = 0
+
+for char in 'true':
+    new_name1 += name1.count(char)
+    new_name2 += name2.count(char)
+     
+for char in 'love':
+    new_name1 += name1.count(char)
+    new_name2 += name2.count(char) 
+
+combination.lower() = int(str(new_name1).lower() + str(new_name2)
+#print(combination)
+
+if combination < 10 or combination > 90
+    print(f"Your score is {combination}, you go together like coke and mentos.")
+
+if combination > 40 and combination < 50:
+    print(f"Your score is {combination}, you are alright together.")
+else:
+    print(f"Your score is {combination},")
