a64e75d80ba6e21b596d03eb9d121aef289412fd
Samuel <kskoomson1@gmail.com>
2023-05-19 21:24:49 +0000

    Gradually getting to the end of beginners, I feel excited, confident and ready to take on further challenges

---
+++ Day_13_of_100/README.md
@@ -0,0 +1,2 @@
+In today's activity, we focus on debugging of codes. 
+Debugging is the identification and correction of errors in a program
---
+++ Day_13_of_100/debugging_odd_or_even.py
@@ -0,0 +1,18 @@
+# Code with bugs
+number = int(input("Which number do you want to check?"))
+
+if number % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+
+#Debugged code is below (the bug was in line 2)
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+    
